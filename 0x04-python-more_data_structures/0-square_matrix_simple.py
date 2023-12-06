129f7ddbad806946de7c0eebd1f258683164bad0
Eddy <eddyukpong@gmail.com>
2023-12-06 05:11:04 -0800

    clean up

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -11,12 +11,3 @@ def square_matrix_simple(matrix=[]):
     return [
         [x**2 for x in row] for row in matrix
     ]
-
-
-if __name__ == '__main__':
-    s = square_matrix_simple([
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9]
-    ])
-    print(s)
