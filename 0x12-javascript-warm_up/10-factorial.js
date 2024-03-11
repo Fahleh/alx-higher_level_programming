b749445971939967422bab462286ba5deaa25764
Eddy <eddyukpong@gmail.com>
2024-03-11 05:30:52 -0700

    Fixes

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,7 +2,7 @@
 const args = process.argv;
 
 const factorial = (n) => {
-  return n === 0 || isNaN(n)? 1 : n * factorial(n - 1);
+  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
 }
 
 console.log(factorial(parseInt(args[2])));
