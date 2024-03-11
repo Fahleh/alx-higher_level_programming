80fc40bd75fce7fd0bce2694fd90e7afff577b0a
Eddy <eddyukpong@gmail.com>
2024-03-11 05:28:19 -0700

    Fixes

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 const args = process.argv;
-const num = parseInt(args[2]);
 
 const factorial = (n) => {
   return n === 0 || isNaN(n)? 1 : n * factorial(n - 1);
 }
 
-console.log(factorial(num));
+console.log(factorial(parseInt(args[2])));
