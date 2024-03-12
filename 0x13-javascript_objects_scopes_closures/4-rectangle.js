47c00531d099c51cd7d68877aee84127e6593894
Eddy <eddyukpong@gmail.com>
2024-03-12 06:39:02 -0700

    Fixes

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -7,13 +7,11 @@ class Rectangle {
     }
   }
 
-
   print () {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
   }
-
 
   rotate () {
     const aux = this.width;
