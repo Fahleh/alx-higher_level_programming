f6c4dc5f60876040aa08cae89c5255998bb25103
Eddy <eddyukpong@gmail.com>
2024-03-27 03:50:21 -0700

    Tasks completed

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+    Defines an algorithm to find the peak in a list
+    of unsorted numbers.
+"""
+
+
+def find_peak(list_of_integers):
+    """Finds a peak in a list of unsorted integers."""
+    if list_of_integers == []:
+        return None
+
+    length = len(list_of_integers)
+    if length == 1:
+        return list_of_integers[0]
+    elif length == 2:
+        return max(list_of_integers)
+
+    mid = int(length / 2)
+    peak = list_of_integers[mid]
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
+    else:
+        return find_peak(list_of_integers[mid + 1:])
